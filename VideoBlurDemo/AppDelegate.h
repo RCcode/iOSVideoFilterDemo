30afb67d83bfa0aade3c0d9fd0c74de034621bbb
MAXToooNG <maxtooong@MAXToooNGdeMacBook-Pro.local>
2015-03-03 17:54:31 +0800

    Initial Commit

---
+++ VideoBlurDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VideoBlurDemo
+//
+//  Created by MAXToooNG on 15/3/3.
+//  Copyright (c) 2015年 Chen.Liu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ VideoBlurDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VideoBlurDemo
+//
+//  Created by MAXToooNG on 15/3/3.
+//  Copyright (c) 2015年 Chen.Liu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
